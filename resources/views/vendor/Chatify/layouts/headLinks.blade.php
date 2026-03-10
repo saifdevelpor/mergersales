2d3e5730c5281eb10be6a839481e3507c408b48c
msaifali <msaifalifarooka131@gmail.com>
2026-03-10 12:17:51 +0500

    project updated

--- resources/views/vendor/Chatify/layouts/headLinks.blade.php
+++ resources/views/vendor/Chatify/layouts/headLinks.blade.php
@@ -7,8 +7,12 @@
 <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.jpeg') }}" />
 <meta name="messenger-color" content="{{ $messengerColor }}">
 <meta name="messenger-theme" content="{{ $dark_mode }}">
-@vite(['resources/js/app.js'])
-
+<!-- Scripts -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="url" content="{{ url('') . '/' . config('chatify.routes.prefix') }}" data-user="{{ Auth::user()->id }}">
 
